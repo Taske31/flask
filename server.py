04711f5be01bc3c146c71865e22c848e3a864713
Лицей-1 <taske9331@gmail.com>
2023-02-06 16:27:33 +0300

    comm1

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mission():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def slogan():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def image_mars():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" href="{url_for('static', filename='css/bootstrap.css')}" />
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img  src="{url_for('static', filename='img/mars.jpg')}"
+                    alt="Здесь должен был быть Марс">
+                    <p class ="alert alert-dark" role="alert">Человечество вырастает из детства.</p>
+                    <p class ="alert alert-success" role="alert">Человечеству мала одна планета.</p>
+                    <p class ="alert alert-light" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                    <p class ="alert alert-warning" role="alert">И начнем с Марса!</p>
+                    <p class ="alert alert-danger" role="alert">Присоединяйся!</p>
+                  </body>
+                </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
+
